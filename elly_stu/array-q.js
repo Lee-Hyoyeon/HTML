3351f0f86448bb7db76c1f46637958ba27065ed4
Lee-Hyoyeon <dlgydus099@gmail.com>
2020-11-21 19:31:30 +0900

    퀴즈

--- elly_stu/array-q.js
+++ elly_stu/array-q.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// Q1. make a string out of an array   //배열을 문자열로
+// Q1. make a string out of an array   //배열을 그냥 문자열로
 {
     const fruits = ['apple', 'banana', 'orange'];
     const result= fruits.join("&");   //  join
@@ -27,7 +27,7 @@
   // Q4. make new array without the first two elements
 {//앞에 두개 빼고 새로운 배열 만들기
     const array = [1, 2, 3, 4, 5];
-    const result = array.slice(2,5) ;//배열의 특정부분 리턴 index 2~4까지 
+    const result = array.slice(2,4+1) ;//배열의 특정부분 리턴 index 2~4까지 
     //split은 배열 자체를 수정하기 때문에 사용 x
     console.log(result);
     console.log(array);
@@ -50,25 +50,48 @@ const students = [
 ];
 
   // Q5. find a student with the score 90
+{/*
+    const result= students.find(function(student){
+        return student.score==90;
+    }); 
+    console.log(result); */
+}
 {
-    
+    const result= students.find((student)=> student.score==90); 
+    console.log(result);
 }
 
-  // Q6. make an array of enrolled students
+  // Q6. make an array of enrolled students 
 {
+    const result= students.filter(function(students){ //true만 리턴 
+        return students.enrolled;
+    })
+    console.log(result);
 }
 
   // Q7. make an array containing only the students' scores
   // result should be: [45, 80, 90, 66, 88]
 {
+    const result= students.map(function(student){
+        return student.score ;
+    } )
+    console.log(result);
 }
 
   // Q8. check if there is a student with the score lower than 50
 {
+    const result= students.some((student)=>student.score<50);
+    console.log(result);    //
+
+    const result2= students.every((student)=>student.score<50);
+    console.log(result2);
+    //조건을 모두 갖춰야 할때 사용 . 모두가 50보다 작은건 아니니까 F
+
 }
 
   // Q9. compute students' average score
 {
+    
 }
 
   // Q10. make a string containing all the scores
