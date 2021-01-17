0315bafbc06e5f807847205b07224170e96f4320
Lee-Hyoyeon <dlgydus099@gmail.com>
2021-01-17 22:35:07 +0900

    json

--- elly_stu/json.js
+++ elly_stu/json.js
@@ -1,12 +1,12 @@
 //JSON 
-//JavaScript Object Notation
+//JavaScript Object Notation 자바스크립트 파일과 관련된 것 
 
 //1. Object to JSON
-//stringify
+//stringify 직렬화 
 let json = JSON.stringify(true);
 console.log(json);
 
-json = JSON.stringify(['apple', 'banana']);     //배열타입처럼 보이고 "" 로 나옴 ->json의 규격사항
+json = JSON.stringify(['apple', 'banana']);     //배열타입처럼 보이면서 "" 로 나옴 ->json의 규격사항이다.
 console.log(json);
 
 const rabbit={
@@ -14,27 +14,29 @@ const rabbit={
     color: 'white',
     size: null,
     birthDate: new Date(),
-
-    jump: () => {   //함수에서는 제외됨 
+    jump: () => {     //함수는 obj에 있는 데이터가 아니므로 제외됨 
         console.log(`${this.name} can jump!!`);
     }
 }
-json=JSON.stringify(rabbit);
+json=JSON.stringify(rabbit); //토끼 오브젝트를 json으로 변환 
 console.log(json);
 
-json=JSON.stringify(rabbit,['name','color','size']);
+                            //replacer 함수 형태혹은 배열형태  
+json=JSON.stringify(rabbit,['name','color','size']);  // 원하는 속성만 골라서 정의하면 이것만 나옴 
 console.log(json);
 
+//call back 함수
 json=JSON.stringify(rabbit,(key, value)=>{
-    console.log(`key: ${key}, value:${value} `)
+    console.log(`key: ${key}, value:${value} `);
     return key ==='name'? 'hyo' : value;
-})
+});
 console.log(json);
 
 
-//2.JSON to Object
+//2.JSON to Object 제이슨을 오브젝트로 변환 
 //parse(json)
 json=JSON.stringify(rabbit);
+
 const obj=JSON.parse(json, ( key,value )=>{   //reviver 콜백 함수? 
     console.log(`key: ${key}, value: ${value}`);
     return key=== 'birthDate'? new Date(value): value;
